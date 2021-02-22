89d5b26b822c3cf1f1e61fb90eecfe6cbaf622ae
GabrielMato <58472077+GabrielMatoBoopetyScoopWoop@users.noreply.github.com>
2021-02-22 12:46:11 +0100

    Add files via upload

---
+++ NiOP Kolekcije/Brkovi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiOP_Kolekcije
+{
+    class Brkovi
+    {
+        string podatak1, podatak2, dodatak;
+        int podatak3;
+        char podatak4;
+
+        public string Podatak1 { get => podatak1; set => podatak1 = value; }
+        public string Podatak2 { get => podatak2; set => podatak2 = value; }
+        public string Dodatak { get => dodatak; set => dodatak = value; }
+        public int Podatak3 { get => podatak3; set => podatak3 = value; }
+        public char Podatak4 { get => podatak4; set => podatak4 = value; }
+
+        public Brkovi()
+        {
+        }
+
+        public Brkovi(string podatak1, string podatak2, int podatak3, char podatak4)
+        {
+            this.podatak1 = podatak1;
+            this.podatak2 = podatak2;
+            this.podatak3 = podatak3;
+            this.podatak4 = podatak4;
+        }
+
+        public string Ispis()
+        {
+            string ispis = Podatak1 + "\t" + Podatak2 + "\t" + Convert.ToString(Podatak3) + "\t" + Convert.ToString(Podatak4)+ "\t" + Dodatak + "\r\n";
+            return ispis;
+        }
+    }
+}
---
+++ NiOP Kolekcije/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace NiOP_Kolekcije
+{
+    public partial class Form1 : Form
+    {
+        List<Brkovi> lista = new List<Brkovi>();
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void Unesi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Brkovi brk = new Brkovi(Podatak1.Text, Podatak2.Text,
+                        Convert.ToInt32(Podatak3.Text),
+                        Convert.ToChar(Podatak4.Text));
+                lista.Add(brk);
+                Podatak1.Clear();
+                Podatak2.Clear();
+                Podatak3.Clear();
+            }
+            catch (Exception Greska)
+            {
+                MessageBox.Show("Greška");
+            }
+        }
+
+        private void Obradi_Click(object sender, EventArgs e)
+        {
+            foreach (Brkovi brk in lista)
+            {
+                if(brk.Podatak4.ToString() == "M")
+                {
+                    brk.Dodatak = "Ima brkove";
+                }
+                else
+                {
+                    brk.Dodatak = "Nema brkove";
+                }
+            }
+        }
+
+        private void Ispisi_Click(object sender, EventArgs e)
+        {
+            TextKutija.Text = "Podatak1" + "\t" + "Podatak2" + "\t" + "Podatak3" + "\t" + "Podatak4" + "\t" + "Dodatak" + "\r\n";
+            foreach (Brkovi brk in lista)
+            {
+                TextKutija.AppendText(brk.Ispis());
+            }
+
+        }
+    }
+}
